4959682943ff58f89c34648e13e2dbbf4589c888
LuisBonii <luisiki99@gmail.com>
2025-08-19 19:27:43 -0300

    feat: create the project

---
+++ test.js
@@ -0,0 +1,17 @@
+
+console.log("Executando testes...");
+
+const assert = require('assert');
+
+function runTests() {
+  try {
+    assert.strictEqual(1 + 1, 2, "1 + 1 = 2");
+    console.log("Todos os testes passaram!");
+    process.exit(0);
+  } catch (error) {
+    console.error("Erro nos testes: ", error.message);
+    process.exit(1);
+  }
+}
+
+runTests();
